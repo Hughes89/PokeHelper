b149b0295b7680e74e0d119cfdbc97e9722b5ecb
Hughes89 <mik3h89@gmail.com>
2016-08-27 02:24:13 -0400

    Cleaned up the functions a bit

--- poke.js
+++ poke.js
@@ -1,7 +1,5 @@
+//How many function
 function howMany(current, goal, transfer) {
-    // if (typeof current === 'string' || typeof goal === 'string') {
-    //   return $('howManyResult').html('<p>Please enter a number.</p>');
-    // }
     var transferCount;
     var finalGoal;
     if (transfer) {
@@ -9,41 +7,55 @@ function howMany(current, goal, transfer) {
         finalGoal = Math.round((goal - transferCount) / 3);
         transfer = false;
     } else {
-        finalGoal = Math.floor((goal - current) / 3);
+        finalGoal = Math.round((goal - current) / 3);
     }
 
     if (goal > ((finalGoal * 3) + transferCount)) {
         finalGoal++;
     }
 
-    return $('.howManyResult').html('<p>You only have to catch ' + finalGoal + ' more pokemon!</p><hr>');
+    return $('.howManyResult').html('<p>You only have to catch <b>' + finalGoal + '</b> more pokemon!</p><hr>');
 }
 
+
+//Poke Evo Function (If transferring as evolving)
 function pidgeyCalc(poke, candy, evolveNum, luckyEgg) {
+	if(poke === 0 ){
+		return $('.calcResults').html('<p>You will get <b>0 XP</b></p><hr>');
+	}
+	if(poke === 1 && candy >= evolveNum) {
+		if(luckyEgg) {
+			return $('.calcResults').html('<p>You will get <b>1000 XP</b>.</p><hr>');
+		}
+		return $('.calcResults').html('<p>You will get <b>500 XP</b>.</p><hr>');
+	}
+	if (poke === 1 && candy < evolveNum) {
+		return $('.calcResults').html('<p>You will get <b>0 XP</b></p><hr>');
+	}
   var evolve = 0;
   var transfer = 0;
   var transferCandy = 0;
-  while (candy > evolveNum) {
-    candy = candy - 12;
+  while (candy >= evolveNum) {
+    candy = candy - evolveNum;
     poke--;
     evolve++;
     candy = candy + 2;
   }
   if (poke + candy > evolveNum) {
-    while (candy < 13) {
+    while (candy < evolveNum + 1) {
       poke--;
       candy++;
       transfer++;
-      if(poke + candy < 13) {
+      if(poke + candy < evolveNum + 1) {
       transfer--;
       poke++;
       if(luckyEgg) {
-      	return $('.calcResults').html('<p>Transfer ' + transfer + ' before using your egg.</p><p>You will get ' + evolve * 500 * 2 + ' XP.</p><hr>');
+      	return $('.calcResults').html('<p>To save time transfer <b>' + transfer + '</b> before using your egg.</p><p>You will get <b>' + evolve * 500 * 2 + ' XP</b>. From evolving <b>' + evolve + '</b> pokemon.</p><hr>');
       } else {
-      	return $('.calcResults').html('<p>You will get ' + evolve * 500 + ' XP.</p><hr>');
+      	return $('.calcResults').html('<p>You will get <b>' + evolve * 500 + '</b> XP. From evolving <b>' + evolve + '</b> pokemon.</p><hr>');
       }
       }
-      if (candy === 12 && poke >= 1) {
+      if (candy === evolveNum && poke >= 1) {
         poke--;
         evolve++;
         candy = 0;
